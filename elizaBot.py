0260d39375b8da57d2593fc5c1d9d5010c23ba7e
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-05-24 08:24:17 +0200

    rm non-eliza commands

--- elizaBot.py
+++ elizaBot.py
@@ -1,13 +1,6 @@
-import json
-from random import choice
-from lxml import objectify
 from eliza import Eliza
-from errbot import botcmd, BotPlugin, PY2
+from errbot import botcmd, BotPlugin
 
-if PY2:
-    from urllib2 import urlopen, quote
-else:
-    from urllib.request import urlopen, quote
 
 class ElizaBot(BotPlugin):
     eliza_daemon = Eliza()
@@ -17,27 +10,3 @@ class ElizaBot(BotPlugin):
         """ El'cheapo shrink for you """
         args = args.strip()
         return self.eliza_daemon.respond(args)
-
-    @botcmd
-    def askus(self, _, __):
-        """ Give us a fun topic to talk about
-            thx to http://chatoms.com/
-        """
-        content = urlopen('http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7').read()
-        return json.loads(content.decode())['text']
-
-    @botcmd
-    def complete(self, _, args):
-        """ Complete the given sentence
-            thx to the awesome google completion
-        """
-        args = args.strip()
-        if not args:
-            return 'Complete what ?'
-
-        content = urlopen('http://google.com/complete/search?q=%s&output=toolbar' % quote(args)).read()
-        xml = objectify.fromstring(content)
-        possibilities = xml.xpath("//toplevel/CompleteSuggestion/suggestion/@data")
-        if possibilities:
-            return choice(possibilities)
-        return 'Hmmm... no answer for that'
